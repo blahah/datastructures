d012a1f17f32378381fff8e64134638636c67659
Richard Smith <richardsmith404@gmail.com>
2013-08-20 00:12:45 +0100

    bump version for Stack

--- lib/datastructures/version.rb
+++ lib/datastructures/version.rb
@@ -3,8 +3,8 @@
 module DataStructures
   module VERSION
     MAJOR = 0
-    MINOR = 1
-    PATCH = 3
+    MINOR = 2
+    PATCH = 0
     BUILD = nil
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.');

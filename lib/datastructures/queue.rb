5037dcda36b561f229bcf6e8fd6fe3fd36812e7d
Richard Smith <richardsmith404@gmail.com>
2013-08-28 22:23:25 +0100

    document the earlier structures

--- lib/datastructures/queue.rb
+++ lib/datastructures/queue.rb
@@ -3,37 +3,45 @@ module DataStructures
   # structure using an array container.
   class Queue
 
+    # Return a new Queue
     def initialize
       self.clear
     end
 
+    # Return the number of elements in the queue
     def size
       @array.size
     end
 
     alias :length :size
 
+    # True if the queue is empty
     def empty?
       @array.empty?
     end
 
+    # Add +:item+ to the back of the queue
     def enqueue item
       @array.push item
     end
 
+    # Remove and return the item at the front of the queue
     def dequeue
       raise "Queue underflow: nothing to dequeue." if self.size == 0
       @array.shift
     end
 
+    # Return the item at the front of the queue
     def front
       @array.first
     end
 
+    # Return the item at the back of the queue
     def back
       @array.last
     end
 
+    # Empty the queue
     def clear
       @array = Array.new
     end
--- lib/datastructures/stack.rb
+++ lib/datastructures/stack.rb
@@ -3,37 +3,45 @@ module DataStructures
   # structure using an array container.
   class Stack
 
+    # Return a new Stack
     def initialize
       self.clear
     end
 
+    # Return the number of elements in the stack
     def size
       @array.size
     end
 
     alias :length :size
 
+    # True if the stack is empty
     def empty?
       @array.empty?
     end
 
+    # Add +:item+ to the top of the stack
     def push item
       @array.push item
     end
 
+    # Remove and return the top item in the stack
     def pop
       raise "Stack underflow: nothing to pop." if self.size == 0
       @array.pop
     end
 
+    # Return the top item in the stack
     def top
       @array.last
     end
 
+    # Return the bottom item in the stack
     def bottom
       @array.first
     end
 
+    # Empty the stack
     def clear
       @array = Array.new
     end
